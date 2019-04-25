0d32c8a0ee0e2271ce2b7baef57ff5c2306feff9
GrantCSMB <36041434+GrantCSMB@users.noreply.github.com>
2019-04-24 20:29:46 -0500

    Add files via upload

---
+++ Congrats/index.js
@@ -0,0 +1,6 @@
+function joey(){
+  $(".body").css("background-image", 'url(joey.png)');
+  $(".body").css("background-size", "770px");
+  $("H1").html("AND HAPPY BIRTHDAY JOEY!!!");
+  $("#hide").remove();
+}
